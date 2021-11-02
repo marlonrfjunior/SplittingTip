47eedb17b5da3e304bf4aa51670846dfb982f0d4
Marlon <marlonrfjunior@outlook.com>
2021-11-02 19:28:09 -0300

    create Functions

--- Tips/Tips/Controllers/CalculatorViewController.swift
+++ Tips/Tips/Controllers/CalculatorViewController.swift
@@ -2,9 +2,65 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
     
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctNutton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    @IBOutlet weak var strepperButton: UIStepper!
+    var percentip : Float = 0.1
+    var resultSplit : Float = 0.0
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+        strepperButton.value = 2
+        strepperButton.minimumValue = 2
+        billTextField.endEditing(false)
     }
 
-
+    @IBAction func tipChanged(_ sender: UIButton) {
+        billTextField.endEditing(true)
+        zeroPctButton.isSelected = false
+        tenPctNutton.isSelected = false
+        twentyPctButton.isSelected = false
+        
+        switch(sender.titleLabel!){
+        case zeroPctButton.titleLabel:
+            percentip = 0.0
+            zeroPctButton.isSelected = true
+            break
+        case tenPctNutton.titleLabel:
+            percentip = 0.10
+            tenPctNutton.isSelected = true
+            break
+        default:
+            percentip = 0.20
+            twentyPctButton.isSelected = true
+            break
+    }
+      
+    }
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        
+        splitNumberLabel.text = String(format: "%.0f", sender.value)
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        print(billTextField.text!)
+        print(splitNumberLabel.text!)
+        print(percentip)
+        resultSplit = (Float(billTextField.text ?? "0.0")! * (percentip + 1.0))/Float(splitNumberLabel.text ?? "0.0")!
+        print(resultSplit)
+        self.performSegue(withIdentifier: "goToResult", sender: self )
+        
+    }
+  
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult"{
+            let destination = segue.destination as! ResultsViewController
+            destination.total = resultSplit
+            destination.settings = "Slpit between \(String(describing: splitNumberLabel.text!)) people, with \(Int(percentip*100))% tip."
+            
+        }
+    }
+    
 }
--- Tips/Tips/Controllers/ResultsViewController.swift
+++ Tips/Tips/Controllers/ResultsViewController.swift
@@ -1,10 +1,19 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
-
+    var total : Float?
+    var settings : String?
+    @IBOutlet weak var settingsLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+        totalLabel.text = String(format: "%.2f", total ?? 0.0)
+        settingsLabel.text = settings ?? "Can't read parameters, try again."
     }
 
-
+    @IBAction func calculate(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
 }
